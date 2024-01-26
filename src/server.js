bf062af78bdcb4e28b22f4bd7998fb2a467f5a91
SergioABH <bedoyasergio3@gmail.com>
2024-01-26 10:25:50 -0500

    Test url2

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:${PORT}/",
+                url: "https://backend2-mern.onrender.com/",
             },
         ],
     },
